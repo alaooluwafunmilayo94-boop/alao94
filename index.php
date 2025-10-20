7310e9c6df86b923168b1b1bc478073b9052fc49
alaooluwafunmilayo94-boop <alaooluwafunmilayo94@gmail.com>
2025-10-20 13:44:24 +0100

    home to home page

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         <div class="logo"></div>
         <nav>
             <ul>
-                <li><a href="#home">Home</a></li>
+                <li><a href="#home">Home Page</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
